8d15a654c510ab434a3557c79fc869b69e3f62f9
Lucas <lukaszkarolmlynarczyk@gmail.com>
2023-05-30 14:36:57 +0200

    test useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import { carsList } from './data';
+import { Button } from '@mui/material';
+
 
 function App() {
+  const [car, setCar] = useState(0);
+  const [showDetails, setShowDetails] = useState(false);
+
+function handle0Click() {
+  setCar(0);
+}
+
+function handle1Click() {
+  setCar(1);
+}
+
+function handle2Click() {
+  setCar(2);
+}
+
+function handle3Click() {
+  setCar(3);
+}
+
+let cars = carsList[car];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <>
+ <div className='app'>
+        <h1>Spis aut, które posiadałem:</h1>
+        <Button variant="contained" size="small" onClick={handle0Click}>Pierwsze</Button>
+        <Button variant="contained" size="small" onClick={handle1Click}>Drugie</Button>
+        <Button variant="contained" size="small" onClick={handle2Click}>Trzecie</Button>
+        <Button variant="contained" size="small" onClick={handle3Click}>Czwarte</Button>
+      
+        <h2>Marka: {cars.brand}</h2>
+        <h3>Model: {cars.model}</h3>
+        <img
+          src={cars.src}
+          alt={cars.alt}
+        />
+        <h4>Rok produkcji: {cars.year}</h4>
+        <h4>Kolor mojego egzemplarza: {cars.color}</h4>
+        <p>{cars.details}</p>
+</div>
+        <footer>
+          Powered by Łukasz Młynarczyk
+        </footer>
+</>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,38 @@
+import matiz from './matiz.png';
+import mazda6 from './mazda6.png';
+import clio from './clio.png';
+import cx5 from './cx5.png';
+
+export const carsList = [{
+brand: 'Daewoo',
+model: 'Matiz',
+color: 'Blue',
+year: '2000',
+src: matiz,
+alt: 'Matiz',
+details: 'Daewoo Matiz - samochód osobowy klasy najmniejszej produkowany pod południowokoreańską marką Daewoo w latach 1998−2008, 1998−2011 w Korei Południowej oraz pod polską marką FSO jako FSO Matiz w latach 2004−2007.'
+}, {
+brand: 'Mazda',
+model: '6',
+color: 'Black',
+year: '2007',
+src: mazda6,
+alt: 'M6',
+details: 'Mazda 6 lub Mazda6 - samochód osobowy klasy średniej produkowany przez koncern Mazda od 2002 roku. Od 2012 roku produkowana jest trzecia generacja modelu.'
+}, {
+brand: 'Renault',
+model: 'Clio',
+color: 'Black',
+year: '2009',
+src: clio,
+alt: 'Clio',
+details: 'Renault Clio - samochód osobowy klasy aut miejskich produkowany pod francuską marką Renault od 1990 roku. Od 2019 roku produkowana jest piąta generacja pojazdu.'
+}, {
+brand: 'Mazda',
+model: 'CX-5',
+color: 'Soul Red',
+year: '2014',
+src: cx5,
+alt: 'CX-5',
+details: 'Mazda CX-5 - samochód osobowy typu SUV klasy średniej produkowany pod japońską marką Mazda od 2012 roku. Od 2017 roku produkowana jest druga generacja pojazdu.'
+}];
