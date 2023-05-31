7f557bbafafb908f54db9882586f6a1763a1a9ae
Lucas <lukaszkarolmlynarczyk@gmail.com>
2023-05-31 14:48:48 +0200

    recoding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import { carsList } from './data';
 import { Button } from '@mui/material';
 
@@ -7,33 +7,36 @@ import { Button } from '@mui/material';
 function App() {
   const [car, setCar] = useState(0);
 
-function handle0Click() {
-  setCar(0);
-}
+  function handlePreviousClick() {
+    if (car > 0) { setCar(car - 1) }
+    else { setCar(0) }
+  }
 
-function handle1Click() {
-  setCar(1);
-}
+  function handleNextClick() {
+    if (car < 3) { setCar(car + 1) }
+    else { setCar(3) }
+  }
 
-function handle2Click() {
-  setCar(2);
-}
-
-function handle3Click() {
-  setCar(3);
-}
-
-let cars = carsList[car];
+  let cars = carsList[car];
 
   return (
- <>
- <div className='app'>
+    <>
+      <div className='app'>
         <h1>Moje dotychczasowe auta:</h1>
-        <Button variant="contained" size="small" onClick={handle0Click}>Pierwsze</Button>
-        <Button variant="contained" size="small" onClick={handle1Click}>Drugie</Button>
-        <Button variant="contained" size="small" onClick={handle2Click}>Trzecie</Button>
-        <Button variant="contained" size="small" onClick={handle3Click}>Czwarte</Button>
-      
+        <Button
+          variant="contained"
+          size="small"
+          onClick={handlePreviousClick}
+          disabled={car < 1}
+        >Starsze</Button>
+        <Button
+          variant="contained"
+          size="small"
+          onClick={handleNextClick}
+          disabled={car > 2}
+        >Nowsze</Button>
+
+
         <h2>Marka: {cars.brand}</h2>
         <h3>Model: {cars.model}</h3>
         <img
@@ -43,11 +46,11 @@ let cars = carsList[car];
         <h4>Rok produkcji: {cars.year}</h4>
         <h4>Kolor mojego egzemplarza: {cars.color}</h4>
         <p>{cars.details}</p>
-</div>
-        <footer>
-          Powered by Łukasz Młynarczyk
-        </footer>
-</>
+      </div>
+      <footer>
+        Powered by Łukasz Młynarczyk
+      </footer>
+    </>
   );
 }
 
